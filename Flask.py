d129ed57f2e7a8602abb83eefe585bda47bd20a3
Sam Gan <gan.jingying@dhs.sg>
2013-05-01 15:31:57 +0800

    lots of other stuff happened

--- Flask.py
+++ Flask.py
@@ -12,14 +12,6 @@ app.secret_key = 'my precious'
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')
-
 def login_required(test):
     @wraps(test)
     def wrap(*args, **kwargs):
@@ -43,16 +35,20 @@ def tasks():
     g.db.close()
     return render_template('tasks.html')
 
-@app.route('/log', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def log():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('hello'))
-    return render_template('log.html', error=error)
+if request.method == 'POST':
+    if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        error = 'Invalid Credentials. Please try again.'
+    else:
+        session['logged_in'] = True
+        return redirect(url_for('tasks'))
+return render_template('log.html', error=error)
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
 
 if __name__ == '__main__':
     app.run(debug=True)
